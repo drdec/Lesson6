e27aa8edf6cbdeee122ef024c49c25f0f8139b55
Dr. Deckto <fynjy138@gmail.com>
2022-01-20 04:44:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] process = Process.GetProcesses();
+
+            for (int i = 0; i < process.Length; i++)
+            {
+                Process process1 = process[i];
+                Console.WriteLine(process1.ProcessName);
+            }
+
+            Console.WriteLine(process.Length);
+        }
+    }
+}
